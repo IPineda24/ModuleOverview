a7184fa260b06df6020165c8c7cf4023b41d1047
Irvin Pineda <irvin.pineda024@gmail.com>
2023-10-27 10:08:33 -0600

    feat: :sparkles: Add Inheritance example :sparkles:

--- repasoPOO.js
+++ repasoPOO.js
@@ -79,3 +79,96 @@ myCar.setModel("Accord");
 console.log(`Make: ${myCar.getMake()}`); 
 console.log(`Model: ${myCar.getModel()}`);
 
+/*
+Inheritance
+
+In JavaScript, "inheritance" refers to the mechanism by which an object
+or class can inherit properties and methods from another object or class.
+This allows for code reuse and the creation of hierarchies of related objects.
+*/
+
+
+class Transport {
+    constructor({
+        brand,
+        model,
+        electric = false,
+        year
+    }) {
+        this.brand = brand;
+        this.model = model;
+        this.electric = electric;
+        //Private variable
+        this._year = year;
+
+        //public methods
+        this.getYear = function () {
+            return this._year;
+        };
+        this.setYear = function (newYear) {
+            let date = new Date();
+            let thisYear = parseInt(date.getFullYear());
+            if (newYear > 1950 && newYear <= thisYear) {
+                this._model = newYear;
+            } else {
+            
+                console.log("sorry but this year is no valid");
+            }
+        };
+    }
+};
+
+class Automobile extends Transport {
+    constructor({
+        brand,
+        model,
+        electric,
+        year,
+        wheels
+    }) {
+        super({ brand, model, electric, year });
+        this.wheels = wheels;
+    }
+};
+
+class Airplane extends Transport {
+    constructor({
+        brand,
+        model,
+        electric,
+        year,
+        engines = 1
+    }) {
+        super({ brand, model, electric, year });
+        this.engines = engines;
+    }
+
+    fly() {
+        console.log(`El avión ${this.brand} ${this.model} está volando.`);
+    }
+};
+
+const newAuto = new Automobile({
+    brand: "Toyota",
+    model: "Supra",
+    wheels: 4
+});
+newAuto.setYear(2023);
+
+
+const newAirplane = new Airplane({
+    brand: "Zivko Aeronautics",
+    model: "Edge 540",
+    year: 1990,
+    engines: 2,
+});
+
+
+//mostrar
+
+console.log(newAuto);
+console.log(newAirplane);
+newAirplane.fly();
+
+
+
